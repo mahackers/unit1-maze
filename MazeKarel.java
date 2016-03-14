efc5618d2c42905e593b711b6408167b1c8e1bc6
Jack Beasley <jack.beasley@gmail.com>
2016-03-14 11:52:52 -0700

    Add Karel Maze starter code and mazes

---
+++ MazeKarel.java
@@ -0,0 +1,9 @@
+import acm.program.*;
+import stanford.karel.*;
+
+public class MazeKarel extends SuperKarel {
+	public void run() {
+       		// Code to control Karel goes here
+
+	}
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+javac -cp .:karel.jar:acm.jar MazeKarel.java
+java -cp .:karel.jar:acm.jar:MazeKarel.class stanford.karel.Karel code=MazeKarel
